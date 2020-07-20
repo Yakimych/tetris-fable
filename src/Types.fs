fbb1df13a16ad58b5329749b16b1622907fba938
Kyrylo Yakymenko <yakimych@gmail.com>
2020-07-20 20:49:52 +0200

    Add shapes for all Piece types

--- src/Types.fs
+++ src/Types.fs
@@ -27,12 +27,143 @@ module PieceI =
         | Left
         | Right ->
             [| [| 0; 0; 0; 0 |]
-               [| 1; 1; 1; 1 |]
                [| 0; 0; 0; 0 |]
+               [| 1; 1; 1; 1 |]
                [| 0; 0; 0; 0 |] |]
+
+module PieceO =
+    let getMatrix (_: Orientation) =
+        [| [| 0; 0; 0; 0 |]
+           [| 0; 1; 1; 0 |]
+           [| 0; 1; 1; 0 |]
+           [| 0; 0; 0; 0 |] |]
+
+module PieceJ =
+    let getMatrix (orientation: Orientation) =
+        match orientation with
+        | Up ->
+            [| [| 0; 0; 0; 0 |]
+               [| 1; 0; 0; 0 |]
+               [| 1; 1; 1; 0 |]
+               [| 0; 0; 0; 0 |] |]
+        | Left ->
+            [| [| 0; 0; 0; 0 |]
+               [| 0; 1; 0; 0 |]
+               [| 0; 1; 0; 0 |]
+               [| 1; 1; 0; 0 |] |]
+        | Down ->
+            [| [| 0; 0; 0; 0 |]
+               [| 0; 0; 0; 0 |]
+               [| 1; 1; 1; 0 |]
+               [| 0; 0; 1; 0 |] |]
+        | Right ->
+            [| [| 0; 0; 0; 0 |]
+               [| 0; 1; 1; 0 |]
+               [| 0; 1; 0; 0 |]
+               [| 0; 1; 0; 0 |] |]
+
+module PieceL =
+    let getMatrix (orientation: Orientation) =
+        match orientation with
+        | Up ->
+            [| [| 0; 0; 0; 0 |]
+               [| 0; 0; 1; 0 |]
+               [| 1; 1; 1; 0 |]
+               [| 0; 0; 0; 0 |] |]
+        | Left ->
+            [| [| 0; 0; 0; 0 |]
+               [| 1; 1; 0; 0 |]
+               [| 0; 1; 0; 0 |]
+               [| 0; 1; 0; 0 |] |]
+        | Down ->
+            [| [| 0; 0; 0; 0 |]
+               [| 0; 0; 0; 0 |]
+               [| 1; 1; 1; 0 |]
+               [| 1; 0; 0; 0 |] |]
+        | Right ->
+            [| [| 0; 0; 0; 0 |]
+               [| 0; 1; 0; 0 |]
+               [| 0; 1; 0; 0 |]
+               [| 0; 1; 1; 0 |] |]
+
+module PieceS =
+    let getMatrix (orientation: Orientation) =
+        match orientation with
+        | Up ->
+            [| [| 0; 0; 0; 0 |]
+               [| 0; 1; 1; 0 |]
+               [| 1; 1; 0; 0 |]
+               [| 0; 0; 0; 0 |] |]
+        | Left ->
+            [| [| 0; 0; 0; 0 |]
+               [| 1; 0; 0; 0 |]
+               [| 1; 1; 0; 0 |]
+               [| 0; 1; 0; 0 |] |]
+        | Down ->
+            [| [| 0; 0; 0; 0 |]
+               [| 0; 0; 0; 0 |]
+               [| 0; 1; 1; 0 |]
+               [| 1; 1; 0; 0 |] |]
+        | Right ->
+            [| [| 0; 0; 0; 0 |]
+               [| 0; 1; 0; 0 |]
+               [| 0; 1; 1; 0 |]
+               [| 0; 0; 1; 0 |] |]
+
+module PieceZ =
+    let getMatrix (orientation: Orientation) =
+        match orientation with
+        | Up ->
+            [| [| 0; 0; 0; 0 |]
+               [| 1; 1; 0; 0 |]
+               [| 0; 1; 1; 0 |]
+               [| 0; 0; 0; 0 |] |]
+        | Left ->
+            [| [| 0; 0; 0; 0 |]
+               [| 0; 1; 0; 0 |]
+               [| 1; 1; 0; 0 |]
+               [| 1; 0; 0; 0 |] |]
+        | Down ->
+            [| [| 0; 0; 0; 0 |]
+               [| 0; 0; 0; 0 |]
+               [| 1; 1; 0; 0 |]
+               [| 0; 1; 1; 0 |] |]
+        | Right ->
+            [| [| 0; 0; 0; 0 |]
+               [| 0; 0; 1; 0 |]
+               [| 0; 1; 1; 0 |]
+               [| 0; 1; 0; 0 |] |]
+
+module PieceT =
+    let getMatrix (orientation: Orientation) =
+        match orientation with
+        | Up ->
+            [| [| 0; 0; 0; 0 |]
+               [| 0; 1; 0; 0 |]
+               [| 1; 1; 1; 0 |]
+               [| 0; 0; 0; 0 |] |]
+        | Left ->
+            [| [| 0; 0; 0; 0 |]
+               [| 0; 1; 0; 0 |]
+               [| 1; 1; 0; 0 |]
+               [| 0; 1; 0; 0 |] |]
+        | Down ->
+            [| [| 0; 0; 0; 0 |]
+               [| 0; 0; 0; 0 |]
+               [| 1; 1; 1; 0 |]
+               [| 0; 1; 0; 0 |] |]
+        | Right ->
+            [| [| 0; 0; 0; 0 |]
+               [| 0; 1; 0; 0 |]
+               [| 0; 1; 1; 0 |]
+               [| 0; 1; 0; 0 |] |]
 
 let getPieceMatrix (pieceShape: PieceShape) (orientation: Orientation) =
     match pieceShape with
     | I -> PieceI.getMatrix orientation
-    // TODO
-    | _ -> PieceI.getMatrix orientation
+    | O -> PieceO.getMatrix orientation
+    | J -> PieceJ.getMatrix orientation
+    | L -> PieceL.getMatrix orientation
+    | S -> PieceS.getMatrix orientation
+    | Z -> PieceZ.getMatrix orientation
+    | T -> PieceT.getMatrix orientation
