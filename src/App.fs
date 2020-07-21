7bc041a388bb2eae74e51a912ce2469325bbce63
Kyrylo Yakymenko <yakimych@gmail.com>
2020-07-21 10:54:12 +0200

    Use ViewBox, update svg when incrementing/decrementing

--- src/App.fs
+++ src/App.fs
@@ -17,7 +17,15 @@ let update (msg: Msg) (state: State): State =
     | Increment -> { state with Count = state.Count + 1 }
     | Decrement -> { state with Count = state.Count - 1 }
 
+[<Literal>]
+let canvasWidth = 800
+
+[<Literal>]
+let canvasHeight = 800
+
 let view (state: State) (dispatch: Msg -> unit) =
+    let height = 10 * state.Count
+    let width = 20 * state.Count
     Html.div
         [ Html.button
             [ prop.onClick (fun _ -> dispatch Increment)
@@ -28,15 +36,24 @@ let view (state: State) (dispatch: Msg -> unit) =
                 prop.text "Decrement" ]
           Html.h1 state.Count
           Html.svg
-              [ //prop.viewBox (0, 0, 100, 100)
-                Html.rect
-                    [ prop.width 100
-                      prop.height 200
-                      prop.x 10
-                      prop.y 20
-                      prop.style [ style.fill "Orange" ]
-                      prop.stroke "Red"
-                      prop.strokeWidth 2 ]
+              [ prop.viewBox (0, 0, canvasWidth, canvasHeight)
+                prop.children
+                    [ Html.rect
+                        [ prop.width canvasWidth
+                          prop.height canvasHeight
+                          prop.x 0
+                          prop.y 0
+                          prop.style [ style.fill "Orange" ]
+                          prop.stroke "Red"
+                          prop.strokeWidth 2 ]
+                      Html.rect
+                          [ prop.width width
+                            prop.height height
+                            prop.x 10
+                            prop.y 10
+                            prop.style [ style.fill "Black" ]
+                            prop.stroke "Yellow"
+                            prop.strokeWidth 2 ] ]
                 unbox ("width", "40%") ] ]
 
 Program.mkSimple init update view
